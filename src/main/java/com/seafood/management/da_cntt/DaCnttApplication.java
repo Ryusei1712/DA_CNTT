ca38bfdf2704ddedbcbb19be89c82393116067e9
K E N <113843247+Ryusei1712@users.noreply.github.com>
2024-05-22 17:46:22 +0700

    First commit

---
+++ src/main/java/com/seafood/management/da_cntt/DaCnttApplication.java
@@ -0,0 +1,13 @@
+package com.seafood.management.da_cntt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaCnttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaCnttApplication.class, args);
+	}
+
+}
